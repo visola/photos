3abcfa459c2b9445fa2795e29119f6505d241407
Vinicius Isola <viniciusisola@gmail.com>
2019-05-19 20:18:20 -0400

    Add Photo model, DAO and SQL with basic save metadata implementation

--- src/main/java/org/visola/lifebooster/controllers/PhotoController.java
+++ src/main/java/org/visola/lifebooster/controllers/PhotoController.java
@@ -1,19 +1,57 @@
 package org.visola.lifebooster.controllers;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.visola.lifebooster.dao.PhotoDao;
+import org.visola.lifebooster.model.Photo;
+
+import com.google.common.hash.Hashing;
 
 @RequestMapping("${api.base.path}/photos")
 @Controller
 public class PhotoController {
 
+  private final PhotoDao photoDao;
+
+  public PhotoController(PhotoDao photoDao) {
+    this.photoDao = photoDao;
+  }
+
   @PostMapping
-  public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile files) {
-    return null;
+  @Transactional
+  public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
+    String hash = Hashing.sha256().hashBytes(file.getBytes()).toString();
+
+    Optional<Photo> maybePhoto = photoDao.findByHash(hash);
+    if (maybePhoto.isPresent()) {
+      return ResponseEntity.ok(maybePhoto.get());
+    }
+
+    Photo photo = new Photo();
+    photo.setName(file.getOriginalFilename());
+    photo.setSize(file.getSize());
+    photo.setUploadedAt(System.currentTimeMillis());
+
+    String path = Hashing.sha256()
+        .hashString(
+            String.format("%s-%d", photo.getName(), photo.getUploadedAt()),
+            StandardCharsets.UTF_8)
+        .toString();
+
+    photo.setPath(path);
+    photo.setHash(hash);
+    photo.setId(photoDao.create(photo));
+
+    return ResponseEntity.ok(photo);
   }
 
 }
---
+++ src/main/java/org/visola/lifebooster/dao/PhotoDao.java
@@ -0,0 +1,24 @@
+package org.visola.lifebooster.dao;
+
+import java.util.Optional;
+
+import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+import org.visola.lifebooster.model.Photo;
+
+public interface PhotoDao {
+
+  @GetGeneratedKeys
+  @SqlUpdate("INSERT INTO photo (hash, name, path, size, uploaded_at)"
+      + " VALUES (:hash, :name, :path, :size, :uploadedAt)")
+  long create(@BindBean Photo photo);
+
+  @RegisterBeanMapper(Photo.class)
+  @SqlQuery("SELECT * FROM photo WHERE hash = :hash")
+  Optional<Photo> findByHash(@Bind("hash") String hash);
+
+}
