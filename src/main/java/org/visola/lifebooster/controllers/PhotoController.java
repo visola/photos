2f3926ef0186da049d40427c72a5bbf5d3dcdb71
Vinicius Isola <viniciusisola@gmail.com>
2019-05-19 20:31:40 -0400

    Upload photo to Google Cloud Storage

--- src/main/java/org/visola/lifebooster/controllers/PhotoController.java
+++ src/main/java/org/visola/lifebooster/controllers/PhotoController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,22 +15,33 @@ import org.springframework.web.multipart.MultipartFile;
 import org.visola.lifebooster.dao.PhotoDao;
 import org.visola.lifebooster.model.Photo;
 
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
 import com.google.common.hash.Hashing;
 
 @RequestMapping("${api.base.path}/photos")
 @Controller
 public class PhotoController {
 
+  private final String bucketName;
   private final PhotoDao photoDao;
+  private final Storage storage;
 
-  public PhotoController(PhotoDao photoDao) {
+  public PhotoController(
+      @Value("${photos.bucket.name}") String bucketName,
+      PhotoDao photoDao,
+      Storage storage) {
+    this.bucketName = bucketName;
     this.photoDao = photoDao;
+    this.storage = storage;
   }
 
   @PostMapping
   @Transactional
   public ResponseEntity<?> uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
-    String hash = Hashing.sha256().hashBytes(file.getBytes()).toString();
+    byte[] bytes = file.getBytes();
+    String hash = Hashing.sha256().hashBytes(bytes).toString();
 
     Optional<Photo> maybePhoto = photoDao.findByHash(hash);
     if (maybePhoto.isPresent()) {
@@ -50,6 +62,12 @@ public class PhotoController {
     photo.setPath(path);
     photo.setHash(hash);
     photo.setId(photoDao.create(photo));
+
+    BlobId blobId = BlobId.of(bucketName, photo.getPath());
+    BlobInfo blobInfo = BlobInfo.newBuilder(blobId)
+        .setContentType(file.getContentType())
+        .build();
+    storage.create(blobInfo, bytes);
 
     return ResponseEntity.ok(photo);
   }
