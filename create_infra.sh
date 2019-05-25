dfa6b0853cde8e82b4f6c9b1aca2e3504afeda11
Vinicius Isola <viniciusisola@gmail.com>
2019-05-25 16:27:10 -0400

    Make deploy script create the buckets and delpoy function

--- create_infra.sh
+++ create_infra.sh
@@ -20,9 +20,17 @@ if [ -z "$2" ]; then
     exit 1
 fi
 
+PROJECT=$1
+IMAGE_BUCKET_NAME=$2
+REGION=us-east1
+
+gsutil mb -c regional -l $REGION gs://$IMAGE_BUCKET_NAME
+gsutil mb -c regional -l $REGION gs://$IMAGE_BUCKET_NAME-thumbnails
+
 gcloud functions deploy generateThumb \
-    --project $1 \
+    --region $REGION \
+    --project $PROJECT \
     --runtime nodejs8 \
     --trigger-bucket $2 \
-    --set-env-vars "THUMBNAIL_BUCKET=$2-thumbnails" \
+    --set-env-vars "THUMBNAIL_BUCKET=$IMAGE_BUCKET_NAME-thumbnails" \
     --source src/main/functions/thumb_generator
