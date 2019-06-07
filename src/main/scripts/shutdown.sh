77d6f8049be01f458711932e2a949df691297dc4
Vinicius Isola <viniciusisola@gmail.com>
2019-06-06 20:44:53 -0400

    Shutdown before deploying

--- src/main/scripts/shutdown.sh
+++ src/main/scripts/shutdown.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PID=$(ps -eF | grep java | grep $1 | awk '{ print $2 }')
+PID=$(ps -eF | grep java | grep life-booster- | awk '{ print $2 }')
 echo "Shutting down process with PID: $PID"
 kill $PID
 
