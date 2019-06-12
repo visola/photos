4e5fa9c8db33bb2608633c134c305d0fff2884fc
Vinicius Isola <viniciusisola@gmail.com>
2019-06-11 21:27:02 -0400

    Remove life-booster classes and references on backend

--- src/main/java/org/visola/lifebooster/config/JDBIConfiguration.java
+++ src/main/java/org/visola/lifebooster/config/JDBIConfiguration.java
@@ -6,12 +6,8 @@ import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.visola.lifebooster.dao.HabitDao;
-import org.visola.lifebooster.dao.JournalEntryDao;
 import org.visola.lifebooster.dao.PhotoDao;
-import org.visola.lifebooster.dao.ProjectDao;
 import org.visola.lifebooster.dao.UserDao;
-import org.visola.lifebooster.model.Photo;
 
 @Configuration
 public class JDBIConfiguration {
@@ -24,23 +20,8 @@ public class JDBIConfiguration {
   }
 
   @Bean
-  public HabitDao habitDao(Jdbi jdbi) {
-    return jdbi.onDemand(HabitDao.class);
-  }
-
-  @Bean
-  public JournalEntryDao journalEntryDao(Jdbi jdbi) {
-    return jdbi.onDemand(JournalEntryDao.class);
-  }
-
-  @Bean
   public PhotoDao photoDao(Jdbi jdbi) {
     return jdbi.onDemand(PhotoDao.class);
-  }
-
-  @Bean
-  public ProjectDao projectDao(Jdbi jdbi) {
-    return jdbi.onDemand(ProjectDao.class);
   }
 
   @Bean
