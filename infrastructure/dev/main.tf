d59065e96281d6d1dee4a3972d2bb97575d38f39
Vinicius Isola <viniciusisola@gmail.com>
2019-06-18 06:04:13 -0400

    Add photos_webapp module to dev

--- infrastructure/dev/main.tf
+++ infrastructure/dev/main.tf
@@ -38,3 +38,26 @@ module "mysql_database" {
   db_password = var.db_password
   environment = var.environment
 }
+
+data "google_dns_managed_zone" "base_domain" {
+  name = var.base_domain_zone_name
+}
+
+module "photos_application" {
+  source = "../modules/photos_webapp"
+
+  domain          = "photos-${var.environment}.${data.google_dns_managed_zone.base_domain.dns_name}"
+  environment     = var.environment
+  tls_certificate = file(var.tls_certificate_file)
+  tls_private_key = file(var.tls_private_key)
+}
+
+resource "google_dns_record_set" "environment_domain" {
+  name = "photos-${var.environment}.${data.google_dns_managed_zone.base_domain.dns_name}"
+  type = "A"
+  ttl  = 300
+
+  managed_zone = "${data.google_dns_managed_zone.base_domain.name}"
+
+  rrdatas = [ module.photos_application.load_balancer_ip_address ]
+}
